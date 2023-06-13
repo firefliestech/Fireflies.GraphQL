b5494ea541709c6cf1f916101fd049cb17d0d184
Lars Stenberg <larste@fireflies.tech>
2023-06-13 12:49:16 +0200

    ClientError now uses Extension["code"] to extract this property to top level

--- Client/Fireflies.GraphQL.Client.Generator/Shared/Error/ClientError.cs
+++ Client/Fireflies.GraphQL.Client.Generator/Shared/Error/ClientError.cs
@@ -1,8 +1,7 @@
 ﻿public class ClientError : IClientError {
     public string Message { get; set; }
-    public string? Code { get; set; }
+    public string? Code => Extensions?["code"]?.ToString();
     public IReadOnlyList<object>? Path { get; set; }
     public IReadOnlyList<Location>? Locations { get; set; }
-    public Exception? Exception { get; set; }
     public IReadOnlyDictionary<string, object?>? Extensions { get; set; }
 }
--- Client/Fireflies.GraphQL.Client.Generator/Shared/Error/IClientError.cs
+++ Client/Fireflies.GraphQL.Client.Generator/Shared/Error/IClientError.cs
@@ -3,6 +3,5 @@
     string? Code { get; }
     IReadOnlyList<object>? Path { get; }
     IReadOnlyList<Location>? Locations { get; }
-    Exception? Exception { get; }
     IReadOnlyDictionary<string, object?>? Extensions { get; }
 }
